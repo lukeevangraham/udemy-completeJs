be2ca7815585a8f6cd3539170d0b413436e68e13
Cefalu Wave <luke.evan.graham@gmail.com>
2020-08-07 23:19:05 -0700

    getting through the hoisting lecture

--- Course-master/3-how-JS-works/starter/script.js
+++ Course-master/3-how-JS-works/starter/script.js
@@ -2,9 +2,34 @@
 // Lecture: Hoisting
 
 
+// functions
+calculateAge(1965);
+
+function calculateAge(year) {
+    console.log(2016 - year);
+}
+
+// retirement(1990)
+
+let retirement = function(year) {
+    console.log(65 - (2016 - year));
+}
 
 
 
+
+// variables
+console.log(age)
+var age = 23;
+
+
+function foo() {
+    console.log(age)
+    var age = 65;
+    console.log(age);
+}
+foo();
+console.log(age)
 
 
 
