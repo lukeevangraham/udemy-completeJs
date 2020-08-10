0bdc12adc81e51f70736b87deeab953771f1377f
Cefalu Wave <luke.evan.graham@gmail.com>
2020-08-09 20:48:55 -0700

    basic game working, no bonus challenges yet

--- Course-master/4-DOM-pig-game/starter/app.js
+++ Course-master/4-DOM-pig-game/starter/app.js
@@ -9,40 +9,36 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer;
+var scores, roundScore, activePlayer, gamePlaying;
 
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
-
-document.querySelector(`.dice`).style.display = "none";
-
-document.getElementById(`score-0`).textContent = "0";
-document.getElementById(`score-1`).textContent = "0";
-document.getElementById(`current-0`).textContent = "0";
-document.getElementById(`current-1`).textContent = "0";
+init();
 
 document.querySelector(`.btn-roll`).addEventListener("click", function () {
-  // 1 Random number
-  var dice = Math.floor(Math.random() * 6) + 1;
+  if (gamePlaying) {
+    // 1 Random number
+    var dice = Math.floor(Math.random() * 6) + 1;
 
-  // 2 Display the result
-  var diceDOM = document.querySelector(`.dice`);
-  diceDOM.style.display = "block";
-  diceDOM.src = "dice-" + dice + ".png";
+    // 2 Display the result
+    var diceDOM = document.querySelector(`.dice`);
+    diceDOM.style.display = "block";
+    diceDOM.src = "dice-" + dice + ".png";
 
-  // 3 Update the round score IF the rolled number was NOT a 1
-  if (dice !== 1) {
-    //Add score
-    roundScore += dice;
-    document.querySelector("#current-" + activePlayer).textContent = roundScore;
-  } else {
-    nextPlayer();
+    // 3 Update the round score IF the rolled number was NOT a 1
+    if (dice !== 1) {
+      //Add score
+      roundScore += dice;
+      document.querySelector(
+        "#current-" + activePlayer
+      ).textContent = roundScore;
+    } else {
+      nextPlayer();
+    }
   }
 });
 
 document.querySelector(`.btn-hold`).addEventListener(`click`, function () {
-  // add current score to player's gloal score
+    if (gamePlaying) {
+        // add current score to player's gloal score
   scores[activePlayer] += roundScore;
 
   // Update the UI
@@ -50,17 +46,47 @@ document.querySelector(`.btn-hold`).addEventListener(`click`, function () {
     scores[activePlayer];
 
   // Check if player won the game
-if (scores[activePlayer] >= 20) {
+  if (scores[activePlayer] >= 100) {
     document.querySelector("#name-" + activePlayer).textContent = "Winner!";
     document.querySelector(".dice").style.display = "none";
-    document.querySelector(".player-" + activePlayer + "-panel").classList.add('winner');
-    document.querySelector(".player-" + activePlayer + "-panel").classList.remove('active');
-} else {
+    document
+      .querySelector(".player-" + activePlayer + "-panel")
+      .classList.add("winner");
+    document
+      .querySelector(".player-" + activePlayer + "-panel")
+      .classList.remove("active");
+    gamePlaying = false;
+  } else {
     // Next player
     nextPlayer();
-}
-
+  }
+    }
 });
+
+document.querySelector(`.btn-new`).addEventListener(`click`, init);
+
+function init() {
+  scores = [0, 0];
+  activePlayer = 0;
+  roundScore = 0;
+
+  gamePlaying = true;
+
+  document.querySelector(`.dice`).style.display = "none";
+
+  document.getElementById(`score-0`).textContent = "0";
+  document.getElementById(`score-1`).textContent = "0";
+  document.getElementById(`current-0`).textContent = "0";
+  document.getElementById(`current-1`).textContent = "0";
+  document.getElementById("name-0").textContent = "Player 1";
+  document.getElementById("name-1").textContent = "Player 2";
+
+  document.querySelector(".player-0-panel").classList.remove("winner");
+  document.querySelector(".player-1-panel").classList.remove("winner");
+  document.querySelector(".player-0-panel").classList.remove("active");
+  document.querySelector(".player-1-panel").classList.remove("active");
+  document.querySelector(".player-0-panel").classList.add("active");
+}
 
 function nextPlayer() {
   //Next player
