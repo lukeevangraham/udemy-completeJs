159e146104ab020ccc19db0261408f095ff3e316
Cefalu Wave <luke.evan.graham@gmail.com>
2020-08-09 19:39:57 -0700

    working on next player advancement

--- Course-master/4-DOM-pig-game/starter/app.js
+++ Course-master/4-DOM-pig-game/starter/app.js
@@ -13,7 +13,7 @@ var scores, roundScore, activePlayer;
 
 scores = [0, 0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
 document.querySelector(`.dice`).style.display = 'none';
 
@@ -34,6 +34,18 @@ document.querySelector(`.btn-roll`).addEventListener('click', function() {
     
 
     // 3 Update the round score IF the rolled number was NOT a 1
+    if (dice !== 1) {
+        //Add score
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+        //Next player
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+    }
 })
 
 
