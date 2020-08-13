f54891ee303485a26b932e42b852d75e16fe2be4
Cefalu Wave <luke.evan.graham@gmail.com>
2020-08-12 17:22:46 -0700

    building the data structure

--- Course-master/6-budgety/starter/app.js
+++ Course-master/6-budgety/starter/app.js
@@ -1,5 +1,44 @@
 // BUDGET CONTROLLER
-var budgetController = (function () {})();
+var budgetController = (function () {
+
+    var Expense = function(id, description, value) {
+        this.id = id;
+        this.description = description,
+        this.value = value
+    };
+    
+    var Income = function(id, description, value) {
+        this.id = id;
+        this.description = description,
+        this.value = value
+    };
+
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    }
+
+    return {
+        addItem: function(type, des, val) {
+            var newItem;
+
+            if (type === 'exp') {
+                newItem = new Expense(ID, des, val);
+
+            } else if (type === 'inc') {
+                newItem = new Income(ID, des, val);
+            }
+        }
+    }
+    
+
+})();
 
 // UI CONTROLLER
 var UIController = (function () {
