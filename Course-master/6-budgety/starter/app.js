36055531c062b060f25868aa769ca6fe95af87d4
Cefalu Wave <luke.evan.graham@gmail.com>
2020-08-12 17:05:35 -0700

    Budgety app work underway

---
+++ Course-master/6-budgety/starter/app.js
@@ -0,0 +1,64 @@
+// BUDGET CONTROLLER
+var budgetController = (function () {})();
+
+// UI CONTROLLER
+var UIController = (function () {
+  var DOMstrings = {
+    inputType: `.add__type`,
+    inputDescription: `.add__description`,
+    inputValue: `.add__value`,
+    inputButton: `.add__btn`,
+  };
+
+  return {
+    getInput: function () {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value, // will be either inc or exp
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value,
+      };
+    },
+
+    getDOMstrings: function () {
+      return DOMstrings;
+    },
+  };
+})();
+
+// GLOBAL APP CONTROLLER
+var controller = (function (budgetCtrl, UICtrl) {
+  var setupEventListeners = function () {
+    var DOM = UICtrl.getDOMstrings();
+    document
+      .querySelector(DOM.inputButton)
+      .addEventListener("click", ctrlAddItem);
+
+    document.addEventListener("keypress", function (event) {
+      if (event.keyBode === 13 || event.which === 13) {
+        ctrlAddItem();
+      }
+    });
+  };
+
+
+
+  var ctrlAddItem = function () {
+    // 1. Get the field input data
+    var input = UICtrl.getInput();
+    // 2. Add the item to the budget controller
+    // 3.  Add the item to the UI
+    // 4.  Calcultate the budget
+    // 5.  Display the budget on the UI
+  };
+
+  return {
+      init: function() {
+          console.log("Application has started.")
+          setupEventListeners();
+      }
+  }
+
+})(budgetController, UIController);
+
+
+controller.init()
